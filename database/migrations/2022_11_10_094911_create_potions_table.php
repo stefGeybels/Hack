460f24f95d6f92ca9d12ab084a6f607a35f9f83d
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2022-11-15 12:39:27 +0100

    updated potion migration
    made name unique
    added description string

--- database/migrations/2022_11_10_094911_create_potions_table.php
+++ database/migrations/2022_11_10_094911_create_potions_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('potions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('description');
             //TODO: ADD PROPERTIES
             $table->timestamps();
         });
