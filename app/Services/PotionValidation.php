16687aecbd5802fd2a08e54d8240a34a8d08455a
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2022-11-15 13:31:06 +0100

    Add logic for potion status calculation

---
+++ app/Services/PotionValidation.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Ingredient;
+
+class PotionValidation
+{
+    public $status;
+    private $statusEnum = [
+        "Stable",
+        "Explosive",
+        "possioness",
+        "weak",
+        "instable",
+    ];
+    public $ingredients = [];
+    private $score;
+
+    public function __construct()
+    {
+        $this->status = "Explosive";
+    }
+
+    public function brew($ingredients)
+    {
+        foreach ($ingredients as $key => $ingredient)
+        {
+            $item = Ingredient::find($ingredient);
+            if ($item != null)
+            {
+                array_push($this->ingredients, $item);
+            }
+            $this->score = $this->score + $item->value;
+        }
+        $this->potionLogic();
+    }
+
+    protected function potionLogic()
+    {
+        if ($this->score % 4 == 0)
+        {
+            $this->status = $this->statusEnum[0];
+            return true;
+        }
+
+        if ($this->score % 3 == 1)
+        {
+            $this->status = $this->statusEnum[2];
+            return true;
+        }
+
+        if ($this->score % 6 == 0)
+        {
+            $this->status = $this->statusEnum[1];
+        }
+
+        if (($this->score - 1) % 3 == 0)
+        {
+            $this->status = $this->statusEnum[4];
+            return true;
+        }
+
+        if ($this->score % 2 == 0)
+        {
+            $this->status = $this->statusEnum[3];
+            return true;
+        }
+
+        return false;
+    }
+}
