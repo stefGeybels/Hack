3524350b7e567ff9333af372b987602fa19f264b
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2022-11-15 15:59:06 +0100

    Updating back-end

--- resources/views/newPotion.blade.php
+++ resources/views/newPotion.blade.php
@@ -12,15 +12,19 @@
     </header>
     <body>
         <div class="background-wizzard-succes">
-            <img src= "{{ asset('/img/background_wizard_room.jpg')}}">
-            <h1 class="h1">You need to be a wizard you made a new potion</h1>
-            <div class="form-save-potion">
-                <label for="fname">Name potion:</label><br>
-                <input type="text" id="namePotion" name="namePotion" value="name"><br>
-                <label for="lname">Last name:</label><br>
-                <textarea id="info-potion" name="info-potion"></textarea>
-                <input type="submit" value="Save potion">
-            </div>
+{{--            <img src= "{{ asset('/img/background_wizard_room.jpg')}}">--}}
+{{--            <h1 class="h1">You need to be a wizard you made a new potion</h1>--}}
+            <form action="/save-potion" method="POST">
+                @csrf
+                <div class="form-save-potion">
+                    <label for="fname">Name potion:</label><br>
+                    <input type="text" id="namePotion" name="name" value="name"><br>
+                    <label for="lname">Last name:</label><br>
+                    <textarea id="info-potion" name="description"></textarea>
+                    <input type="submit" value="Save potion">
+                </div>
+            </form>
+
         </div>
 
     </body>
